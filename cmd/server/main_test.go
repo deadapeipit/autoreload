baf366b0c93651df1422885253645763c4c10628
deadapeipit <davidroring28@gmail.com>
2022-07-25 11:20:07 +0700

    add testcase write json

---
+++ cmd/server/main_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"autoreload/model"
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const jsonPath = "html/weatherstatus.json"
+
+var wstat model.WeatherStatus
+
+func Test_writefile(t *testing.T) {
+	t.Log("Test write json file")
+	//check is debug true
+	wd, _ := os.Getwd()
+
+	if strings.Contains(wd, "cmd") {
+		os.Chdir("..")
+		os.Chdir("..")
+	}
+	file, err := ioutil.ReadFile(jsonPath)
+	assert.NotNil(t, file)
+	assert.NoError(t, err)
+	err = json.Unmarshal(file, &wstat)
+	assert.NotNil(t, wstat)
+	assert.NoError(t, err)
+}
